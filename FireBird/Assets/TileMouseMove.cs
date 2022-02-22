b22d0480e8cb182a7f17735a91c8ab01328d049a
CapnTarder <twc17a@acu.edu>
2022-02-22 02:05:25 -0600

    tile hover works

--- FireBird/Assets/TileMouseMove.cs
+++ FireBird/Assets/TileMouseMove.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class TileMouseMove : MonoBehaviour
 {
+    Color normalColor;
+    public Color highLightColor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        normalColor = GetComponent<Renderer>().material.color;
     }
 
 
-
-
+   
 
 
 
@@ -21,10 +22,33 @@ public class TileMouseMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i< Input.touchCount; i++)
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if(GetComponent<Collider>().Raycast( ray, out hitInfo, Mathf.Infinity  ))
         {
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
-                Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+            GetComponent<Renderer>().material.color = highLightColor;
+
         }
+        else
+        {
+            GetComponent<Renderer>().material.color = normalColor;
+        }
+
+
+
+
+
+
+
+
+
+
+
+      //  for (int i = 0; i< Input.touchCount; i++)
+       // {
+      //      Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+      //          Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+      //  }
     }
 }
