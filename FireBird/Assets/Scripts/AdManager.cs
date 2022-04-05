41b8379936e311003b84e6eb30d688d54d8ef31c
CapnTarder <twc17a@acu.edu>
2022-04-05 11:59:37 -0500

    cringeLaptop2

---
+++ FireBird/Assets/Scripts/AdManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+    // android: your_id
+    // apple: your_id
+    //string gameId = "your_id";
+    bool testMode = true;
+
+#if UNITY_IOS
+        private string gameId = "4687120";
+#elif UNITY_ANDROID
+        private string gameId = "4687121";
+#endif
+
+    void Start()
+    {
+        // Initialize the Ads service:
+        //Advertisement.Initialize(gameId);
+        Advertisement.Initialize(gameId, testMode);
+    }
+
+    public void ShowInterstitialAd()
+    {
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show("GoHome");
+        }
+        else
+        {
+            Debug.Log("Interstitial ad not ready at the moment! Please try again later!");
+        }
+        //Time.timeScale = 0f;
+    }
+
+}
